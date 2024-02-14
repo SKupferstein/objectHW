08a3afb3f758df4712cc1f14fccfa401de96f798
Shifra Kupferstein <shk56893@gmail.com>
2024-02-14 12:34:53 +0200

    newer version

--- Objects.js
+++ Objects.js
@@ -3,32 +3,26 @@ const sara = {
   lastName: "Miller",
   mass: 70,
   height: 1.55,
-  calcBMI: (bmi = function () {
+  calcBMI: function () {
+    this.bmi = (this.mass / (this.height * 2)).toFixed(2);
     return (this.mass / (this.height * 2)).toFixed(2);
-  }),
+  },
 };
 const malka = {
   firstName: "Malka",
   lastName: "Smith",
   mass: 92,
   height: 1.85,
-  calcBMI: (bmi = function () {
+  calcBMI: function () {
+    this.bmi = (this.mass / (this.height * 2)).toFixed(2);
     return (this.mass / (this.height * 2)).toFixed(2);
-  }),
+  },
 };
 if (sara.calcBMI() > malka.calcBMI())
   console.log(
-    `${sara.firstName} ${
-      sara.lastName
-    }'s BMI (${sara.calcBMI()}) is greater then ${malka.firstName} ${
-      malka.lastName
-    }'s BMI (${malka.calcBMI()}).`
+    `${sara.firstName} ${sara.lastName}'s BMI ${sara.bmi} is greater then ${malka.firstName} ${malka.lastName}'s BMI ${malka.bmi}.`
   );
 else
   console.log(
-    `${malka.firstName} ${
-      malka.lastName
-    }'s BMI (${malka.calcBMI()}) is greater then ${sara.firstName} ${
-      sara.lastName
-    }'s BMI (${sara.calcBMI()}).`
+    `${malka.firstName} ${malka.lastName}'s BMI ${malka.bmi} is greater then ${sara.firstName} ${sara.lastName}'s BMI ${sara.bmi}.`
   );
